5ed6d34242e4d29a413a16e8b84746e81b498e04
redouin-elitesdsi <redouin.alvarez@elitesdsi.com>
2024-03-25 16:53:37 +0800

    Added configs for netrw in red.core

--- lua/red/core/init.lua
+++ lua/red/core/init.lua
@@ -4,6 +4,8 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.g.mapleader = " "
+vim.g.netrw_keepdir = 0
+vim.g.netrw_banner = 0
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-s>", vim.cmd.w)
